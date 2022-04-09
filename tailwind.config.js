634932ff70d07958374244599e780f3f345ad0ad
Léo GRAND <leograndcontact@gmail.com>
2022-04-09 06:17:36 +0200

    Ajout de la FAQ + theme switcher

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
 		require('@tailwindcss/aspect-ratio'),
 		require('daisyui'),
 	],
+	theme: {
+		extend: {
+			fontSize: {
+				'tiny': '.655rem',
+			}
+		}
+	},
 	daisyui: {
 		themes: [
 			{
@@ -12,7 +19,17 @@ module.exports = {
 					...require("daisyui/src/colors/themes")["[data-theme=dark]"],
 					accent: "#00e849",
 					"accent-focus": "#009830",
+					primary: "#00e849",
+					"primary-focus": "#00e849",
 					"base-100": "#272a30"
+				},
+				light: {
+					...require("daisyui/src/colors/themes")["[data-theme=autumn]"],
+					accent: "#a60dff",
+					"accent-focus": "#6f00b0",
+					primary: "#a60dff",
+					"primary-focus": "#a60dff",
+					// "base-100": "#272a30"
 				},
 			},
 		],
