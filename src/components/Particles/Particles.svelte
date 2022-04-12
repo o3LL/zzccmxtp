2e52a7196a31ec505ece80e0b0ec61067b391666
Léo GRAND <leograndcontact@gmail.com>
2022-04-12 05:46:26 +0200

    Cleanup particles

--- src/components/Particles/Particles.svelte
+++ src/components/Particles/Particles.svelte
@@ -1,41 +1,26 @@
 <script>
   import Particles from "svelte-particles";
-  import { tsParticles } from "tsparticles";
+  import config from './config';
 
-  import configPoo from './configPoo.json';
-  import configZz from './configZz.json';
-
-  tsParticles.load("tsparticlesPoo", configPoo)
-  tsParticles.load("tsparticlesZz", configZz)
   let onParticlesLoaded = (event) => {
     const particlesContainer = event.detail.particles;
-    console.log(event)
     // you can use particlesContainer to call all the Container class
     // (from the core library) methods like play, pause, refresh, start, stop
   };
 
   let onParticlesInit = async (event) => {
     const main = event.detail;
-    console.log(event)
     // you can use main to customize the tsParticles instance adding presets or custom shapes
     // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
     // starting from v2 you can add only the features you need reducing the bundle size
-    await tsParticles.load("tsparticlesPoo", main);
   };
 </script>
 
 <div class="absolute top-0 bottom-0 left-0 right-0 h-full w-full -z-10">
   <Particles
-    id="tsparticlesPoo"
-    options="{configPoo}"
+    id="tsparticles"
+    options="{config}"
     on:particlesLoaded="{onParticlesLoaded}"
-    particlesInit="{onParticlesInit}"
-  />
-
-  <Particles
-    id="tsparticlesZz"
-    options="{configZz}"
-    on:particlesLoaded="{onParticlesLoaded}"
-    particlesInit="{onParticlesInit}"
-  />
+    on:particlesInit="{onParticlesInit}"
+  ></Particles>
 </div>
---
+++ src/components/Particles/config.ts
@@ -0,0 +1,89 @@
+import { RecursivePartial, IOptions } from 'tsparticles';
+
+const config: RecursivePartial<IOptions> = {
+  fullScreen: {
+    enable: true,
+    zIndex: -10,
+  },
+  particles: {
+    number: {
+      value: 0.4,
+      density: {
+        enable: true,
+        area: 9.469771699587272,
+      },
+    },
+    color: {
+      value: "#5fff7b",
+    },
+    shape: {
+      type: "image",
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
+      image: [
+        {
+          src: "/zz.webp",
+          width: 80,
+          height: 80,
+        },
+        {
+          src: "/poo.webp",
+          width: 80,
+          height: 80,
+        },
+      ],
+    },
+    opacity: {
+      value: 1,
+      random: true,
+    },
+    size: {
+      value: 15.782952832645451,
+      random: true,
+    },
+    move: {
+      enable: true,
+      speed: { min: 0.1, max: 4},
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: true,
+        rotateX: 8417.913319543995,
+        rotateY: 6814.5012586784715,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "window",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse",
+      },
+      onclick: {
+        mode: 'push',
+        enable: true,
+      },
+      resize: true,
+    },
+    modes: {
+      repulse: {
+        distance: 60,
+        duration: 0.1,
+        speed: 1
+      },
+      push: {
+        quantity: 2
+      }
+    },
+  },
+  fpsLimit: 120,
+  retina_detect: true,
+};
+
+export default config;
