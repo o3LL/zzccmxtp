b39dd5d7f6889c56ded804815ef2104c74b6c1aa
Léo GRAND <leograndcontact@gmail.com>
2022-06-15 18:22:22 +0200

    Achete la prod

--- src/components/Player/AudioLeaks.svelte
+++ src/components/Player/AudioLeaks.svelte
@@ -60,7 +60,7 @@
     { title: "37. JRPG (Ft. Baek, Epektase & LE GRAND JD)", url: "/audio/leaks/Un_peu_de_cash" },
     { title: "38. Le milli (je crois)", url: "/audio/leaks/Panpan-construct" },
     { title: "40. 3 Zèbres (Ft. Ysos)", url: "/audio/leaks/3_Zebres" },
-    { title: "bizarre. Eva Elfie (bah c sulfitman)", url: "/audio/leaks/sulfi-eva_elfi" },
+    { title: "bizarre. Eva Elfie (bah c sulfitman)", url: "/audio/leaks/sulfi-eva_elfi", sulfi: true },
     { title: "?. Jc pas", url: "/audio/leaks/Jc_pas" },
     { title: "?. J'ai rien à leur dire", url: "/audio/leaks/g_rien_a_leur_dire" },
     { title: "?. Interlude Deums", url: "/audio/leaks/Interlude_Deums" },
--- src/components/Player/Media.svelte
+++ src/components/Player/Media.svelte
@@ -5,6 +5,7 @@
   export let title;
   export let url;
   export let volume;
+  export let sulfi;
   $: computedVolume = volume / 100;
 
   const cardStyle =
@@ -50,6 +51,31 @@
         <source src={`${url}.mp3`} type="audio/mpeg" />
         <source src={`${url}.ogg`} type="audio/ogg" />
       </audio>
+      {#if sulfi}
+        <a
+        class="tooltip tooltip-primary ml-2 justify-end z-10"
+        href="https://twitter.com/sulfiteman"
+        data-tip={`Télécharger ${title}`}
+        onclick="alert('Achete la prod non?'); event.stopPropagation();"
+      >
+        <button class="btn btn-square btn-outline">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
+            />
+          </svg>
+        </button>
+      </a>
+      {:else}
       <a
         class="tooltip tooltip-primary ml-2 justify-end z-10"
         href={`${url}.mp3`}
@@ -74,6 +100,7 @@
           </svg>
         </button>
       </a>
+      {/if}
     </figure>
     <slot />
   </div>
