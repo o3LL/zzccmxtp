3be9c48bf8726b230c56dccace7c69b3a2d5bc48
Léo GRAND <leograndcontact@gmail.com>
2022-06-17 00:18:57 +0200

    ECOUTE PUTIAN

--- src/components/CountDown.svelte
+++ src/components/CountDown.svelte
@@ -1,88 +1,28 @@
-<script>
-  export let end;
-
-  let done = false;
-  let dayTime, hoursTime, minutesTime, secondsTime;
-  let dayTimeCss = "--value:99";
-  let hoursTimeCss = "--value:99";
-  let minutesTimeCss = "--value:99";
-  let secondsTimeCss = "--value:99";
-  const seconds = 1000;
-  const minutes = seconds * 60;
-  const hours = minutes * 60;
-  const days = hours * 24;
-
-  const endTime = new Date(end).getTime();
-
-  const interval = setInterval(function () {
-    let now = new Date().getTime();
-    const difference = endTime - now;
-
-    if (difference < 0) {
-      clearInterval(interval);
-      done = true;
-      return;
-    }
-
-    dayTime = Math.floor(difference / days);
-    dayTimeCss = `--value:${dayTime}`;
-
-    hoursTime = Math.floor((difference % days) / hours);
-    hoursTimeCss = `--value:${hoursTime}`;
-
-    minutesTime = Math.floor((difference % hours) / minutes);
-    minutesTimeCss = `--value:${minutesTime}`;
-
-    secondsTime = Math.floor((difference % minutes) / seconds);
-    secondsTimeCss = `--value:${secondsTime}`;
-  }, seconds);
-</script>
-
 <div>
-  {#if !done}
-    <h3 class="text-lg">La tape sort dans :</h3>
-
-    <div class="grid grid-flow-col gap-5 text-center auto-cols-max">
-      <div class="flex flex-col">
-        <span class="countdown font-mono text-5xl">
-          <span style={dayTimeCss} />
-        </span>
-        jours
-      </div>
-
-      <div class="flex flex-col">
-        <span class="countdown font-mono text-5xl">
-          <span style={hoursTimeCss} />
-        </span>
-        heurs
-      </div>
-
-      <div class="flex flex-col">
-        <span class="countdown font-mono text-5xl">
-          <span style={minutesTimeCss} />
-        </span>
-        min
-      </div>
-
-      <div class="flex flex-col">
-        <span class="countdown font-mono text-5xl">
-          <span style={secondsTimeCss} />
-        </span>
-        sec
-      </div>
-    </div>
-  {/if}
-  {#if done}
-    <div class="alert shadow-lg">
+  <a
+    href="https://bfan.link/zzccmxtp"
+    class=""
+    target="_blank">
+    <button class="btn btn-primary btn-lg h-64">
       <div>
-        <h3 class="text-lg">
-          la tape est sorti mon chégé va voir sur <a
-            href="https://zzcc.fr/mxtp"
+        <h3 class="text-3xl">
+          ecoutee cette fuckin tape sur ton réso préféré !! <a
+            href="https://bfan.link/zzccmxtp"
             class="link"
-            target="_blank">le vrai site</a
-          > !
+            target="_blank">en cliquan ici</a
+          > !  <a
+            href="https://bfan.link/zzccmxtp"
+            class="link"
+            target="_blank">ou la!!</a
+          >
+
+          voir meme  <a
+            href="https://bfan.link/zzccmxtp"
+            class="link"
+            target="_blank">ici !!</a
+          >
         </h3>
       </div>
-    </div>
-  {/if}
+    </button>
+  </a>
 </div>
