85e5f6a9c306c8c959d4678f96864fc74f69b184
Léo GRAND <leograndcontact@gmail.com>
2022-11-07 02:59:40 +0100

    Les ptit fanart ça arrive fort

---
+++ src/components/FanartForm.svelte
@@ -0,0 +1,4 @@
+<div>
+<script async src="https://tally.so/widgets/embed.js"></script>
+<button class="btn btn-sm btn-accent text-black block mx-auto" data-tally-open="31W421" data-tally-emoji-text="👋" data-tally-emoji-animation="wave">Envoyer mon fanart</button>
+</div>
