ad61d4ba35a94c3a50082e9894773d10132f05ad
Léo GRAND <leograndcontact@gmail.com>
2022-11-08 19:36:25 +0100

    LES FANARTS 🎨

--- src/components/FanartForm.svelte
+++ src/components/FanartForm.svelte
@@ -1,4 +1,4 @@
 <div>
 <script async src="https://tally.so/widgets/embed.js"></script>
-<button class="btn btn-sm btn-accent text-black block mx-auto" data-tally-open="31W421" data-tally-emoji-text="👋" data-tally-emoji-animation="wave">Envoyer mon fanart</button>
+<button class="btn btn-xl btn-accent block mx-auto" data-tally-open="31W421" data-tally-emoji-text="👋" data-tally-emoji-animation="wave">Envoyer mon fanart</button>
 </div>
